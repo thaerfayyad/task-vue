4213a5cd910693030fe8985b0006d09ab3324ad0
thaerfayyad <eng.thaer94@gmail.com>
2021-11-28 16:10:26 +0200

    fc

---
+++ src/components/Todo.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+    <div>
+      <div v-for="todo in todos" :key="todo.id">
+        <!-- <h3>{{ todo.title }} {{'the key is : '+ todo.id }}</h3> -->
+        <TodoItem v-bind:todo="todo" v-on:del-todo="$emit('del-todo',todo.id)" />
+        <h4></h4>
+      </div>
+    </div>
+  </div>
+</template> 
+
+<script>
+import TodoItem from '@/components/TodoItem';
+export default {
+  name: "Todos",
+  components: {
+      TodoItem
+  },
+  props: ["todos"],
+};
+</script>
+<style scoped></style>
---
+++ src/components/TodoItem.vue
@@ -0,0 +1,34 @@
+<template>
+  <div class="todo-item" v-bind:class="{ 'is-complete': !todo.completed }">
+    <p>
+      <input type="checkbox" v-on:change="markComplete" />
+      {{ todo.title }}
+      <button class="del" @click="$emit('del-todo',todo.id)">x</button>
+    </p>
+  </div>
+</template>
+<script>
+export default {
+  name: "TodoItem",
+  props: ["todo"],
+  methods: {
+    markComplete() {
+      this.todo.completed = !this.todo.completed;
+    },
+  },
+};
+</script>
+<style scoped>
+.todo-item {
+  background: #f4f4f4;
+  padding: 10px;
+  border-bottom: 1px;
+}
+.is-complete {
+  text-decoration: line-through;
+}
+.del {
+  background: red;
+}
+</style>
+>
